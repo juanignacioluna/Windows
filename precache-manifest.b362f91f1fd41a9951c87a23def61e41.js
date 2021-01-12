e1bdd9810cd657aad430807886879ae5e6ed8394
Juan Ignacio Luna <chinoluna11@gmail.com>
2021-01-12 02:40:29 -0300

    Updates

---
+++ precache-manifest.b362f91f1fd41a9951c87a23def61e41.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132ace37782d4027f388aefacf2583fe",
+    "url": "/Windows/index.html"
+  },
+  {
+    "revision": "77095e4beccf7ef011a3",
+    "url": "/Windows/static/css/main.eea7b021.chunk.css"
+  },
+  {
+    "revision": "6adff9df04dd5c322ff5",
+    "url": "/Windows/static/js/2.0f336560.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/Windows/static/js/2.0f336560.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77095e4beccf7ef011a3",
+    "url": "/Windows/static/js/main.b3b8918d.chunk.js"
+  },
+  {
+    "revision": "34728f09dbd7c297b16c",
+    "url": "/Windows/static/js/runtime-main.1a544d5b.js"
+  },
+  {
+    "revision": "def7fc76d9fee3a1c7c2c7561ff83c9e",
+    "url": "/Windows/static/media/anotador1.def7fc76.png"
+  },
+  {
+    "revision": "c72c5f6ad0b6c6f77bdd7c06c9b1696e",
+    "url": "/Windows/static/media/fondo2.c72c5f6a.jpg"
+  },
+  {
+    "revision": "710cc2d94d374387910ec7e56799a543",
+    "url": "/Windows/static/media/logo1.710cc2d9.png"
+  },
+  {
+    "revision": "e01c13a1da0653cc065095deb5561066",
+    "url": "/Windows/static/media/paint1.e01c13a1.png"
+  }
+]);
